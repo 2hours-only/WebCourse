beaeff9d1b9bf7f6f626aca56286d644bff38c52
2hours-only <356556465@qq.com>
2026-07-03 17:24:58 +0800

    完善初始状态

--- js/recommend/rule.js
+++ js/recommend/rule.js
@@ -3,7 +3,6 @@ export class RuleEngine {
     console.log("[Recommend] RuleEngine created");
   }
 
-  // 接口定义：applyRules(userPreference, seats)
   applyRules(userPreference, seats) {
     console.log("[Recommend] applyRules called", userPreference);
     return seats;
